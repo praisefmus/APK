913c65b30f52a4ac3d971c9c260d760b88b57015
Praise FM <fmpraiseradio@gmail.com>
2025-10-29 17:55:42 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('praisefm-cache').then(cache => {
+      return cache.addAll(['/', '/index.html', '/manifest.json']);
+    })
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
